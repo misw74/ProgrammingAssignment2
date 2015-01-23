628415b9874e6081b6137095878d7946ec417eca
misw74 <misw74@gmail.com>
2015-01-23 22:12:26 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## create caching matrix
+makeCacheMatrix <- function(aMatrix = matrix()) {
+  matrix <- aMatrix ## 'global' matrix
+  inverse <- NULL   ## 'global' cache
+  
+  ## setter, set matrix and clean cache
+  setMatrix <- function( aMatrix) {
+    # store it in the 'global' matrix
+    matrix <<- aMatrix
+    
+    # clean 'global' inverse
+    inverse <<- NULL
+  }
+  
+  ## matrix getter
+  getMatrix <- function() { 
+    matrix
+  }
+  
+  ## inverse setter
+  setInverse <- function( anInverse ) {
+    # store it in the 'global' inverse
+    inverse <<- anInverse
+  }
+  
+  ## inverse getter
+  getInverse <- function() {
+    inverse
+  }
+  
+  ## return Caching Matrix, it became sort of 'object' with methods and data (matrix, inverse)
+  list( setMatrix = setMatrix,
+        getMatrix = getMatrix,
+        setInverse = setInverse,
+        getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## calculate inverse of a caching matrix
+## use inverse from the cache if the cache not empty
+cacheSolve <- function(aMatrix, ...) {
+  myInverse <- aMatrix$getInverse()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if( is.null(myInverse) ) {
+      ## cache is empty
+      ## calculate
+      myInverse <- solve( aMatrix$getMatrix() )
+      
+      ## save for later
+      aMatrix$setInverse( myInverse )    
+    }
+    
+  # return inverse
+  myInverse    
 }
